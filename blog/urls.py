bb3565ae9e3bf579ec947f2554b6f441c2d22bf4
teglanna <teglanna@gmail.com>
2015-06-11 13:50:01 +0200

    Added horse.py and balls.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+	url(r'^$', views.home),
     url(r'^$', views.post_list),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail),
+    url(r'^python/$', views.python, name='python'),
+    url(r'^javascript/$', views.javascript, name='javascript'),
+    url(r'^learning_stuffs/$', views.learning_stuffs, name='learning_stuffs'),
+    url(r'^balls/$', views.balls, name='balls'),
+    url(r'^horse/$', views.horse, name='horse'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from .models import Post
+
+def home(request):
+    return render(request, 'blog/home.html')
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+def python(request):
+	return render(request, 'blog/python.html', {})
+
+def javascript(request):
+	return render(request, 'blog/javascript.html')
+
+def learning_stuffs(request):
+	return render(request, 'blog/learning_stuffs.html')
+
+def balls(request):
+	return render(request, 'blog/balls.html')
+
+def horse(request):
+	return render(request, 'blog/horse.html')
